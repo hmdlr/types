132174b4796a2c4664dd7706f10bf931b5b2e622
cristimc8 <cristimc8@gmail.com>
2022-12-08 21:34:39 +0200

    feat: export `buildPagedResults` function (#14)

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { PagedResults } from './PagedResults';
 import { SortDirection } from './SortDirection';
 import { IUser } from './IUser';
 import { UUID } from './UUID';
+import { buildPagedResults } from './PagedRequest';
 
 export {
   PagedResults,
+  buildPagedResults,
   SortDirection,
   UUID,
   Email,
