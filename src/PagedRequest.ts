4a6ac15277f277bf8915cf1b241bf1981693cfe5
Cristi <cristimc8@gmail.com>
2022-12-10 18:22:51 +0200

    fix: make `buildPagedResults` stable to use for any input

--- src/PagedRequest.ts
+++ src/PagedRequest.ts
@@ -2,15 +2,57 @@ import { Request } from 'express';
 import { PagedResults } from './PagedResults';
 import { SortDirection } from './SortDirection';
 
-export const buildPagedResults = <T>(req: Request): PagedResults<T> => {
+/**
+ * Builds a PagedResults object from the request.
+ * @param req The request.
+ * Accepted query parameters: <br />
+ * ***pageNumber***: The page number. <br />
+ * ***pageSize***: The page size. <br />
+ * ***sortBy***: The column to sort by. Should be (keyof T) array, separated by comma.
+ * If a string is not part of the type's keys, it will just be discarded.  <br />
+ * ***sortDir***: The direction to sort by. Should be SortDirection array, separated by comma  <br />
+ */
+export function buildPagedResults<T>(
+  req?: Request<unknown, unknown, unknown, {
+    pageNumber?: number; pageSize?: number; sortBy?: string; sortDir?: string;
+  }>
+): PagedResults<T>;
+export function buildPagedResults<T>({
+  query: {
+    pageNumber,
+    pageSize,
+    sortBy,
+    sortDir,
+  },
+} :
+{
+  query?: {
+    pageNumber?: number;
+    pageSize?: number;
+    sortBy?: string;
+    sortDir?: string;
+  } }): PagedResults<T>;
+
+export function buildPagedResults<T>(
+  reqOrOpts?: Request<unknown, unknown, unknown, {
+    pageNumber?: number; pageSize?: number; sortBy?: string; sortDir?: string;
+  }> | {
+    query?: {
+      pageNumber?: number;
+      pageSize?: number;
+      sortBy?: string;
+      sortDir?: string;
+    } }
+): PagedResults<T> {
   const pagedResults = new PagedResults<T>();
-  pagedResults.pageNumber = Number(req.query.pageNumber) ?? 1;
-  pagedResults.pageSize = Number(req.query.pageSize) ?? 50;
-  const sortBy = (req.query.sortBy as string).split(',') ?? [];
-  const sortDir = (req.query.sortDir as string).split(',')
-    .map(
-      (dir) => (dir.toLowerCase() === 'desc' ? SortDirection.Desc : SortDirection.Desc)
+
+  pagedResults.pageNumber = parseInt(reqOrOpts?.query?.pageNumber?.toString() ?? '1', 10);
+  pagedResults.pageSize = parseInt(reqOrOpts?.query?.pageSize?.toString() ?? '50', 10);
+  const sortBy = reqOrOpts?.query?.sortBy?.split(',') ?? [];
+  const sortDir = reqOrOpts?.query?.sortDir?.split(',')
+    ?.map(
+      (dir) => (dir.toLowerCase() === 'desc' ? SortDirection.Desc : SortDirection.Asc)
     ) ?? [];
   pagedResults.sort(sortBy as (keyof T)[], sortDir);
   return pagedResults;
-};
+}
