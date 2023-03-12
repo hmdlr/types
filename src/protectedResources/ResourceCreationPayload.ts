979167dfae1dd1ab03c931a752b967779ac899d6
Cristi <cristimc8@gmail.com>
2023-03-12 21:06:38 +0200

    chore: add either group or owner id for resources

--- src/protectedResources/ResourceCreationPayload.ts
+++ src/protectedResources/ResourceCreationPayload.ts
@@ -2,5 +2,8 @@ export interface ResourceCreationPayload {
   id: string;
 
   /* The owner will be the first member of the group created for the specified resource */
-  ownerId: string;
+  ownerId?: string;
+
+  /* If specified, the resource will be created in the specified group */
+  groupId?: string;
 }
