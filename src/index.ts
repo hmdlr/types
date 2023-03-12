7aeba234436706a498583c14f6bfc63e8e0e4e8b
Cristi <cristimc8@gmail.com>
2023-03-12 18:32:53 +0200

    chore: add Resource generic

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import { IWebsiteResult } from './IWebsiteResult';
 import { IBrand } from './brands/IBrand';
 import { IBrandSummary } from './brands/IBrandSummary';
 import { IBrandCreatePayload } from './brands/IBrandCreatePayload';
+import { Resource } from './protectedResources/Resource';
 
 export {
   PagedResults,
@@ -32,5 +33,6 @@ export {
   IWebsiteResult,
   IBrand,
   IBrandSummary,
-  IBrandCreatePayload
+  IBrandCreatePayload,
+  Resource
 };
