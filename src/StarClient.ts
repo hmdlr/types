3f3907c743e2f247b8ee5a4f71139ef7eb8f8cc9
Cristi <cristimc8@gmail.com>
2023-04-07 00:15:04 +0300

    chore: StarClient to extend BareClient

--- src/StarClient.ts
+++ src/StarClient.ts
@@ -1,13 +1,11 @@
-import { AxiosResponse } from 'axios';
-
-export interface StarClient {
-  get: <T>(url: string, options?: any) => Promise<AxiosResponse<T>>;
-  post: <T>(url: string, data: any, options?: any) => Promise<AxiosResponse<T>>;
-  put: <T>(url: string, data: any, options?: any) => Promise<AxiosResponse<T>>;
-  delete: <T>(url: string, options?: any) => Promise<AxiosResponse<T>>;
+export interface BareClient {
+  get: <T>(url: string, options?: any) => Promise<T>;
+  post: <T>(url: string, data: any, options?: any) => Promise<T>;
+  put: <T>(url: string, data: any, options?: any) => Promise<T>;
+  delete: <T>(url: string, options?: any) => Promise<T>;
 }
 
-export interface BareClient {
+export interface StarClient extends BareClient {
   get: <T>(url: string, options?: any) => Promise<T>;
   post: <T>(url: string, data: any, options?: any) => Promise<T>;
   put: <T>(url: string, data: any, options?: any) => Promise<T>;
