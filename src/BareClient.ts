3c85a528c2095720da123b90a839d3537ca5f9fe
Cristi <cristimc8@gmail.com>
2023-04-07 10:26:21 +0300

    chore: add AxiosClient

--- src/BareClient.ts
+++ src/BareClient.ts
@@ -1,6 +1,15 @@
+import { AxiosResponse } from 'axios';
+
 export interface BareClient {
   get: <T>(url: string, options?: any) => Promise<T>;
   post: <T>(url: string, data: any, options?: any) => Promise<T>;
   put: <T>(url: string, data: any, options?: any) => Promise<T>;
   delete: <T>(url: string, options?: any) => Promise<T>;
+}
+
+export interface AxiosClient {
+  get: <T>(url: string, options?: any) => Promise<AxiosResponse<T>>
+  post: <T>(url: string, data: any, options?: any) => Promise<AxiosResponse<T>>
+  put: <T>(url: string, data: any, options?: any) => Promise<AxiosResponse<T>>
+  delete: <T>(url: string, options?: any) => Promise<AxiosResponse<T>>
 }
