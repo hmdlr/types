e5cf777a763afc5d0ed39c9c6447f717abe7afd4
Cristi <cristimc8@gmail.com>
2023-04-08 19:28:03 +0300

    chore: add brands option to IConfig

--- src/configs/IConfig.ts
+++ src/configs/IConfig.ts
@@ -1,7 +1,9 @@
 import { Resource } from '../protectedResources/Resource';
+import { IBrand } from '../brands/IBrand';
 
 export interface IConfig extends Resource {
   id: string;
   name: string;
   creatorId: string;
+  brands: IBrand[];
 }
