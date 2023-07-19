664b0d66d66c980b2ad1b07086218dc92cdea62a
Cristi <cristimc8@gmail.com>
2023-07-19 14:23:12 +0300

    fix: add null check to paged results

--- src/PagedResults.ts
+++ src/PagedResults.ts
@@ -72,7 +72,7 @@ export class PagedResults<T> {
 
   get items(): Array<T> {
     const start = (this.pageNumber - 1) * this.pageSize;
-    const end = this.pageSize !== -1 ? start + this.pageSize : this._items.length;
+    const end = this.pageSize !== -1 ? start + this.pageSize : this._items?.length || 0;
     return this._items.slice(start, end);
   }
 
