f3af84050470edc9bd1bcd8c2903fe4f65973e42
cristi <cristi@localhost.localdomain>
2024-02-15 22:17:02 +0200

    chore: add hasOwnText property to html scanner

--- src/scan/Collection.ts
+++ src/scan/Collection.ts
@@ -44,6 +44,7 @@ export interface HtmlCollection {
     texts: string[]; // text found in the page
     pageTitle: string; // title of the page
     crossDomainForm: boolean; // does the page have a form that points to a different domain?
+    hasOwnText: boolean; // does the page have its own visible, selectible text?
   };
 }
 
